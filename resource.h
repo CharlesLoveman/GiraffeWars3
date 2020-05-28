74667a04d06c0319354fdd58716805dcdacd37ed
Charlie Loveman <charlie@lovemans.co.uk>
2020-05-28 11:02:55 +0100

    Remove unnecessary resources

--- resource.h
+++ resource.h
@@ -2,16 +2,13 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDR_JUMP1                       101
-#define IDR_JUMP2                       102
-#define IDR_HIT1                        103
 #define IDR_HIT2                        104
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
